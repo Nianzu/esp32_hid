5b61f71f4b8564e7f7d1fdd11b3aded9625e9bc7
Nico <nazucca7@gmail.com>
2026-01-24 12:07:53 -0700

    removed clipboard

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,9 @@
 #![no_std]
 
-mod clipboard;
 mod hid_report_writer;
 mod synergy_hid;
 
 pub mod constants;
-pub use clipboard::{button_task, set_clipboard};
 pub use hid_report_writer::{HidReport, send_hid_report, start_hid_task};
 pub use synergy_hid::{ReportType, SynergyHid, ASCII_2_HID, KeyboardReport};
 
